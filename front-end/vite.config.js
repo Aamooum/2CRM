28ce90004d0b6fd5b22571473a90f90c18a95063
Aamooum <younes.aamoum10@gmail.com>
2025-09-24 20:04:54 +0100

    fix: Add production host to Vite config

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    preview: {
+      host: '0.0.0.0',
+      port: process.env.PORT,
+      allowedHosts: [
+        env.VITE_ALLOWED_HOST
+      ]
     }
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT,
-    allowedHosts: [
-      env.VITE_ALLOWED_HOST
-    ]
-  }
+  };
 });
